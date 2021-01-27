bfa8c47a86798b2cbcc6fcd30ddb4f1c3cc75e87
Christian Mulato <chmulato@gmail.com>
2021-01-27 10:22:23 -0300

    Implementando o padrão DTO para Categorias.

--- src/main/java/com/caracore/bookstore/service/CategoriaService.java
+++ src/main/java/com/caracore/bookstore/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.caracore.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +13,15 @@ import com.caracore.bookstore.repositories.CategoriaRepository;
 public class CategoriaService {
 
 	@Autowired
-	private CategoriaRepository categoriaRepository;
+	private CategoriaRepository repository;
 
 	public Categoria findById(Integer id) {
-		Optional<Categoria> obj = categoriaRepository.findById(id);
+		Optional<Categoria> obj = repository.findById(id);
 		return obj.orElse(null);
+	}
+
+	public List<Categoria> findAll() {
+		return repository.findAll();
 	}
 
 }
